181a980212ebd3c062737bd6082e0c9114f8c62d
MAHBUBUL ALAM <mahbublalam500@gmail.com>
2023-10-18 08:51:07 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     lineHeight: {
@@ -64,7 +65,6 @@ export default {
   },
   plugins: [
     require("daisyui"),
-    require('flowbite/plugin')
   ],
 
 }
